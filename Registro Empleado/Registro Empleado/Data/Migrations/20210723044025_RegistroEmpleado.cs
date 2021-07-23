940d441dd5c938cc2d015076cf89473f5b18238c
RikaZa <jrr.zambrano@yavirac.edu.ec>
2021-07-23 00:01:26 -0500

    Regist >

---
+++ Registro Empleado/Registro Empleado/Data/Migrations/20210723044025_RegistroEmpleado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Registro_Empleado.Data.Migrations
+{
+    public partial class RegistroEmpleado : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empleado",
+                columns: table => new
+                {
+                    EmpleadoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Contacto = table.Column<int>(type: "int", nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleado", x => x.EmpleadoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empleado");
+        }
+    }
+}
